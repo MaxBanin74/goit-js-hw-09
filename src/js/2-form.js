1383d990ff62ef43a54a6f78b9658437feb73431
Max Banin <banin@ukr.net>
2026-05-09 15:31:24 +0300

    1

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+let formData = { email: '', message: '' };
+const KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+if (form) {
+  form.addEventListener('input', onInput);
+  form.addEventListener('submit', onSubmit);
+  formData = JSON.parse(localStorage.getItem(KEY)) || {
+    email: '',
+    message: '',
+  };
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+function onInput(event) {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(KEY);
+  formData.email = '';
+  formData.message = '';
+  if (form) form.reset();
+}
